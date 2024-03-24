f425da63c7e31589a9446aa322c268acb4e3f9fc
ALEEZAJOGIYAT <alizajogyat09@gmail.com>
2024-03-25 01:15:28 +0500

    updated git credentials

--- config_files/index.js
+++ config_files/index.js
@@ -1,14 +1,13 @@
 require("dotenv").config(); //instatiate environment variables
 
 let config = {}; //Make this global to use all over the application
-
-// `mongodb+srv://${config.DB_USER}:${config.DB_PASSWORD}@cluster0.yfjcchu.mongodb.net/${config.DB_NAME}?retryWrites=true&w=majority`;
-config.DB_USER = process.env.DB_USER || "aleeza";
-config.DB_NAME = process.env.DB_NAME || "CRM";
-config.DB_PASSWORD = process.env.DB_PASSWORD || "q2Tt77RFz7NHqiz";
+config.DB_USER = process.env.DB_USER || "vlookin_dev";
+config.DB_NAME = process.env.DB_NAME || "Vlookin";
+config.DB_PASSWORD = process.env.DB_PASSWORD || "TcMvTF6gqWWbvbwp";
 config.MONGODB_URI =
   process.env.MONGODB_URI ||
-  `mongodb+srv://${config.DB_USER}:${config.DB_PASSWORD}@cluster0.nbtxw.mongodb.net/${config.DB_NAME}?retryWrites=true&w=majority`
+  'mongodb+srv://vlookin_dev:TcMvTF6gqWWbvbwp@cluster0.dr7t8q8.mongodb.net/Vlookin?retryWrites=true&w=majority'
+  // `mongodb+srv://${config.DB_USER}:${config.DB_PASSWORD}@cluster0.yfjcchu.mongodb.net/${config.DB_NAME}?retryWrites=true&w=majority`
 
 //JWT CONFIG
 config.JWT_SECRET = process.env.JWT_SECRET || "VLookingAppHerkou";
